0bbe165e9fdeb610a6b4a20794d4965d57b62003
Ignacio Molina <molodygs@gmail.com>
2025-08-04 12:00:42 -0400

    refactor: better coding practices

--- src/Energy.cpp
+++ src/Energy.cpp
@@ -4,41 +4,41 @@ namespace Energy
 {
   double currentKineticEnergy = 0.0; //!< Current kinetic energy
   double lastKineticEnergy = 0.0; //!< Last kinetic energy
-}
 
-double Energy::getCurrentKineticEnergy() { return currentKineticEnergy; } //!< Get current kinetic energy
-void Energy::setCurrentKineticEnergy(double energy) { currentKineticEnergy = energy; } //!< Set current kinetic energy
-double Energy::getLastKineticEnergy() { return lastKineticEnergy; } //!< Get last kinetic energy
-void Energy::setLastKineticEnergy(double energy) { lastKineticEnergy = energy; } //!< Set last kinetic energy
-void Energy::setLastKineticEnergyAsCurrent() { currentKineticEnergy = lastKineticEnergy; } //!< Set last kinetic energy as current
-double Energy::deltaKineticEnergy() { return (currentKineticEnergy - lastKineticEnergy); } //!< Set last kinetic energy as current
+  double getCurrentKineticEnergy() { return currentKineticEnergy; } //!< Get current kinetic energy
+  void setCurrentKineticEnergy(double energy) { currentKineticEnergy = energy; } //!< Set current kinetic energy
+  double getLastKineticEnergy() { return lastKineticEnergy; } //!< Get last kinetic energy
+  void setLastKineticEnergy(double energy) { lastKineticEnergy = energy; } //!< Set last kinetic energy
+  void setLastKineticEnergyAsCurrent() { currentKineticEnergy = lastKineticEnergy; } //!< Set last kinetic energy as current
+  double deltaKineticEnergy() { return (currentKineticEnergy - lastKineticEnergy); } //!< Set last kinetic energy as current
 
-void Energy::computeKineticEnergy(vector<Body*>* bodies)
-{
-  // initial value for energy
-  double energy = 0.0;
+  void computeKineticEnergy(vector<Body*>* bodies)
+  {
+    // initial value for energy
+    double energy = 0.0;
 
-  // for each body
-  for (size_t ibody = 0; ibody < bodies->size(); ++ibody) {
+    // for each body
+    for (size_t ibody = 0; ibody < bodies->size(); ++ibody) {
 
-    // get particles
-    vector<Particle*>* particles = bodies->at(ibody)->getParticles();
+      // get particles
+      vector<Particle*>* particles = bodies->at(ibody)->getParticles();
 
-    // for each particle
-#pragma omp parallel for reduction(+:energy) shared(particles)
-    for (int i = 0; i < static_cast<int>(particles->size()); ++i) {
+      // for each particle
+      #pragma omp parallel for reduction(+:energy) shared(particles)
+      for (int i = 0; i < static_cast<int>(particles->size()); ++i) {
 
-      // verify active particle
-      if (!particles->at(i)->getActive()) { continue; }
+        // verify active particle
+        if (!particles->at(i)->getActive()) { continue; }
 
-      // get mass and velocity
-      const double mass = particles->at(i)->getMass();
-      const Vector3d velocity = particles->at(i)->getVelocity();
+        // get mass and velocity
+        const double mass = particles->at(i)->getMass();
+        const Vector3d velocity = particles->at(i)->getVelocity();
 
-      // compute the particle kinetic energy contribution
-      energy += 0.5 * mass * (velocity.x() * velocity.x() + velocity.y() * velocity.y() + velocity.z() * velocity.z());
+        // compute the particle kinetic energy contribution
+        energy += 0.5 * mass * (velocity.x() * velocity.x() + velocity.y() * velocity.y() + velocity.z() * velocity.z());
+      }
     }
-  }
 
-  Energy::setCurrentKineticEnergy(energy);
+    setCurrentKineticEnergy(energy);
+  }
 }
