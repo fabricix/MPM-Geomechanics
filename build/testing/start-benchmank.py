da72366a2b3f0af6191bc12b1062df08cb703e23
Ignacio Molina <molodygs@gmail.com>
2025-09-10 14:26:19 -0300

    refactor<start-benchmark.py>: reorganize executable handling and improve folder structure for benchmarks

--- build/testing/start-benchmank.py
+++ build/testing/start-benchmank.py
@@ -18,50 +18,64 @@ json_template = {
 }
 
 # Executable file names
-executables = {
-    "atomic": {"path": "MPM-Geomechanics-benchmark-atomic", "active": False},
-    "main": {"path": "MPM-Geomechanics-benchmark-main", "active": False},
-    "local": {"path": "MPM-Geomechanics-benchmark-local", "active": False}
-}
+executables = {}
 
-materials_point = [150, 250, 500] # Materials points in **thousands**
-threads = [1, 5, 10] # Numbers of threads
+# Materials points in **thousands**
+materials_point = [150, 250, 500] 
+
+# Numbers of threads
+threads = [1, 5, 10] 
+
+CONFIG_FOLDER = "benchmark/configuration-files"
+LOGS_FOLDER = "logs"
 
 # Configuration file name generator
 def config_file (p, t):
-  folder = Path("benchmark")
-  folder.mkdir(parents=True, exist_ok=True)
-  return folder / f"config-p{p}k-t{t}"
+  return f"config-p{p}k-t{t}"
+
+def get_logs_path(name):
+  return f"benchmark/{name}"
+
+def get_executable_name(executable_path):
+  return executable_path.split('/')[-1]
+
+def create_folders():
+  Path("benchmark").mkdir(parents=True, exist_ok=True)
+  Path(f"{CONFIG_FOLDER}").mkdir(parents=True, exist_ok=True)
+  for executable_path in executables.values():
+    executable_name = get_executable_name(executable_path)
+    Path(f"{get_logs_path(executable_name)}").mkdir(parents=True, exist_ok=True)
 
 # Run a benchmark with a specific executable and configuration file
-def run_benchmark(executable_exe, executable_name, config_file):
-  print(f"[INFO] Running config file: {config_file}.json with executable: {executable_exe}")
+def run_benchmark(executable_path, name, config_file):
+  print(f"[INFO] [{name}] Running config file: {config_file}.json with executable: {get_executable_name(executable_path)}")
   try:
     response = subprocess.run(
-      f'{executable_exe} {config_file}.json > {config_file}-{executable_name}.log',
+      f'{executable_path} {CONFIG_FOLDER}/{config_file}.json > {get_logs_path(get_executable_name(executable_path))}/{config_file}-{name}.log',
       text=True,
-      shell=True
+      shell=True  
     )
     return response
   except Exception as e:
     print(f"[ERROR] An error occurred while running the benchmark: {e}")
-    print(f"[ERROR INFO] Executable: {executable_exe} | Config File: {config_file}")
-    return None
+    print(f"[ERROR] Executable: {executable_path} | Config File: {config_file}")
+    sys.exit(-1)
 
 # Run all the benchmarks of a specific executable
-def execute_benchmarks(executable, executable_name):
-  print(f"[INFO] Starting benchmarks for executable: {executable_name}")
+def execute_benchmarks(executable_path, name):
+  print(f"[INFO] [{name}] Starting benchmarks")
   for p in materials_point:
     for t in threads:
-      run_benchmark(executable, executable_name, config_file(p, t))
-  print(f"[INFO] Completed benchmarks for executable: {executable_name}\n")
+      run_benchmark(executable_path, name, config_file(p, t))
+  print(f"[INFO] [{name}] Completed benchmarks\n")
 
 def create_configuration_files():
+    create_folders()
     for p in materials_point:
         for t in threads:
             json_template["num_particles"] = p * 1000
             json_template["num_threads"] = t
-            with open(f"{config_file(p, t)}.json", "w") as f:
+            with open(f"{CONFIG_FOLDER}/{config_file(p, t)}.json", "w") as f:
                 json.dump(json_template, f, indent=4)
 
 def read_parameters_from_console():
@@ -69,68 +83,50 @@ def read_parameters_from_console():
 
       for parameter in sys.argv[1:]:
         split_parameter = parameter.split(":")
-        exe_name = split_parameter[0]
-
         if len(split_parameter) > 1:
-            path = split_parameter[1]
-        elif exe_name in executables.keys():
-            path = executables[exe_name]["path"]
-
-        if exe_name in executables.keys():
-          executables[exe_name]["active"] = True
-          executables[exe_name]["path"] = path
-          print(f"[INFO] Enabled executable for {exe_name} | path: {path}")
-        else: 
-          print(f"[INFO] The parameter provided is not valid: {parameter}")
-
+        
+          exe_name = split_parameter[0]
+          path = split_parameter[1].split(".exe")[0]
+          executables[exe_name] = path
+          
     else: 
-      print(f"[INFO] No parameters found")
-      for executable in executables.values():
-        executable["active"] = True
+      print(f"[ERROR] No parameters found. Please provide at least one executable in the format: <name>:<path-to-executable>")
+      sys.exit(-1)
 
     # Adjust executable paths
     if sys.platform == "win32":
         print("[INFO] Windows platform detected")
         for exe in executables.keys():
-            executables[exe]["path"] = f"{executables[exe]['path']}.exe"
-            if executables[exe]["active"] and not os.path.exists(executables[exe]["path"]):
-                print(f"[ERROR] Executable not found for {exe}: {executables[exe]['path']}")
-                executables[exe]["active"] = False
+            if not os.path.exists(f"{executables[exe]}.exe"):
+                print(f"[ERROR] Executable not found for {exe}: {executables[exe]}")
+                sys.exit(-1)
 
-    if sys.platform == "linux":
+    if sys.platform == "linux" or sys.platform == "cygwin":
         print("[INFO] Linux platform detected")
         script_dir = os.path.dirname(os.path.abspath(__file__))
         for exe in executables.keys():
-            executables[exe]["path"] = os.path.join(script_dir, executables[exe]["path"])
-            if executables[exe]["active"] and not os.path.exists(executables[exe]["path"]):
-                print(f"[ERROR] Executable not found for {exe}: {executables[exe]['path']}")
-                executables[exe]["active"] = False
+            executables[exe] = os.path.join(script_dir, executables[exe])
+            if not os.path.exists(executables[exe]):
+                print(f"[ERROR] Executable not found for {exe}: {executables[exe]}")
+                sys.exit(-1)
 
 def start_benchmarks():
 
     # Start time measurement
     start_time = time.time()
 
-    if executables["main"]["active"]:
-      thread01 = threading.Thread(target=execute_benchmarks, args=(executables["main"]["path"], "main"))
-      thread01.start()
+    if executables.values():
+      print(f"[INFO] Starting benchmarks with executables: {', '.join(executables.keys())}")
 
-    if executables["atomic"]["active"]:
-        thread02 = threading.Thread(target=execute_benchmarks, args=(executables["atomic"]["path"], "atomic"))
-        thread02.start()
+    # Create and start threads for each executable
+    threads = []
+    for name, executable_path in executables.items():
+      thread = threading.Thread(target=execute_benchmarks, args=(executable_path, name))
+      thread.start()
+      threads.append(thread)
 
-    if executables["local"]["active"]:
-        thread03 = threading.Thread(target=execute_benchmarks, args=(executables["local"]["path"], "local"))
-        thread03.start()
-
-    if executables["main"]["active"]:
-        thread01.join()
-
-    if executables["atomic"]["active"]:
-        thread02.join()
-
-    if executables["local"]["active"]:
-        thread03.join()
+    for thread in threads:
+      thread.join()
 
     # End time measurement
     end_time = time.time()
@@ -140,15 +136,16 @@ def start_benchmarks():
 # Main function
 def main():
 
-
-    # Create configuration files
-    create_configuration_files()
-
     # Read parameters from the console
     read_parameters_from_console()
+
+    # Create configuration files 
+    create_configuration_files()
 
     # Start benchmarking
     start_benchmarks()
 
-# Start benchmarking
+    return 0
+
+# Start benchmarking process
 main()
